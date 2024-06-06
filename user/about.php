da0cfcfe358aa17bb92f8cc30d247ae8fbb71f37
kyy72 <rifkikurniawan210903@gmail.com>
2024-06-06 16:31:08 +0700

    git remote add origin https://github.com/Stunsol/Stuntsolutions.git

--- user/about.php
+++ user/about.php
@@ -2,72 +2,96 @@
     <div id="customCarousel" class="carousel slide" data-ride="carousel">
         <div style="width: 100%">
             <ol style="background: none" class="breadcrumb float-sm-right">
-                <li class="breadcrumb-item"><a href="index.php">Home</a></li>
-                <li class="breadcrumb-item active">About</li>
             </ol>
         </div>
         <div class="carousel-inner">
         <div class="carousel-item active">
             <div class="container ">
             <div class="row">
-                <div class="col-md-5">
                 <div class="detail-box">
-                    <h1>
-                    Informasi tentang kami
-                    <br>
+                <div class="detail-box">
+                    <h1 style="text-align: justify;">
                     StuntSolutions
                     </h1>
-                    <p>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus quidem maiores perspiciatis, illo maxime voluptatem a itaque suscipit.
+                    <p style="text-align: justify;">
+                    Selamat datang di StuntSolutions, platform khusus yang bertujuan untuk meningkatkan kesadaran dan memberikan informasi 
+                    berharga tentang stunting. Misi kami adalah mendidik, memberi informasi, dan memobilisasi masyarakat untuk mengambil tindakan 
+                    melawan stunting, yang merupakan masalah kesehatan masyarakat penting yang mempengaruhi jutaan anak di seluruh dunia.
                     </p>
-                    </a>
+                    <p style="text-align: justify;">
+                    Tujuan kita <br>
+                    Memberikan informasi yang komprehensif mengenai penyebab, akibat, dan pencegahan stunting.
+                    Untuk berbagi penelitian dan data terbaru mengenai stunting untuk membantu masyarakat dan pembuat kebijakan dalam mengambil keputusan.
+                    </p>
+                    
+                    <div class="btn-box">
                     </div>
                 </div>
                 </div>
+                <div class="col-md-7">
+                </div>
             </div>
             </div>
         </div>
+        <div class="carousel-item">
+            <div class="container ">
+            <div class="row">
+                <div class="detail-box">
+                <div class="detail-box">
+                    <h1 style="text-align: justify;">
+                    Pengguna
+                    </h1>
+               <p style="text-align: justify;">
+                   POSYANDU <BR>
+                   Posyandu
+                   Pencatatan dan pelaporan penimbangan anak oleh kader mudah melalui Website StuntSolutions
+                    </p>
+                <p style="text-align: justify;">
+                   Masyarakat/Orang Tua<BR>
+                   Cek status gizi anak dengan mudah. Dan pantau hasil penimbangan anak. Dan pelajari pencegahan stunting melalui aplikasi.
+                    </p>
+
+                   <p style="text-align: justify;">
+                   Desa/Puskesmas<BR>
+                   Monitoring, evaluasi dan kelola data penimbangan anak dari posyandu secara lebih efektif, real time dan paper less.
+                    </p>
+                </div>
+                </div>
+                <div class="col-md-7">
+                </div>
+            </div>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <div class="container ">
+            <div class="row">
+                <div class="detail-box">
+                <div class="detail-box">
+                    <h1 style="text-align: justify;">
+                    TEAM KAMI
+                    </h1>
+               <p style="text-align: justify;">
+                   Muhammad Tegar Abubakar (F2566YB048)
+                    </p>
+                <p style="text-align: justify;">
+                    Rifki Kurniawan (F3156YB292)
+                  
+                   <p style="text-align: justify;">
+                   Hana Silviana Sutanto (F6206XB229)
+                </div>
+                </div>
+                <div class="col-md-7">
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+        <ol class="carousel-indicators">
+        <li data-target="#customCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#customCarousel" data-slide-to="1"></li>
+        <li data-target="#customCarousel" data-slide-to="2"></li>
+        </ol>
     </div>
+    <br><br><br>
 </section>
 
-<section class="contact_section  long_section">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-6">
-          <div class="form_container">
-            <div class="heading_container">
-              <h2>
-                Contact Us
-              </h2>
-            </div>
-            <form action="">
-              <div>
-                <input type="text" placeholder="Your Name" />
-              </div>
-              <div>
-                <input type="text" placeholder="Phone Number" />
-              </div>
-              <div>
-                <input type="email" placeholder="Email" />
-              </div>
-              <div>
-                <input type="text" class="message-box" placeholder="Message" />
-              </div>
-              <div class="btn_box">
-                <button>
-                  SEND
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-        <div class="col-md-6">
-          <div class="map_container">
-            <div class="map">
-              <div id="googleMap"></div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
